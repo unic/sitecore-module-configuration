2aefa07bae1e5179e68a661857e72c5fc30c27d8
Tobias Studer <tobias.studer@unic.com>
2014-01-22 22:53:02 +0100

    Added nunit project and .runsettings to exclude test dll in code coverage reports.

---
+++ tests/Unic.Configuration.Tests/Converter/ConverterFactoryTests.cs
@@ -0,0 +1,22 @@
+﻿namespace Unic.Configuration.Tests.Converter
+{
+    using NUnit.Framework;
+    using Unic.Configuration.Converter;
+
+    public class ConverterFactoryTests
+    {
+        [TestFixture]
+        public class TheGetMethod
+        {
+            [Test]
+            public void CanFindBooleanConverter()
+            {
+                // act
+                var converter = ConverterFactory.Get(typeof(bool));
+
+                // assert
+                Assert.IsInstanceOf<BooleanConverter>(converter);
+            }
+        }
+    }
+}
